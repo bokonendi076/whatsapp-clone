3785efc8d1801937c93bafd79351de61ad4e08cb
pro0448 <48685330+pro0448@users.noreply.github.com>
2022-11-29 13:51:42 +0100

    Login  file update

--- login/whatsapp-login.php
+++ login/whatsapp-login.php
@@ -1,2 +1,16 @@
 <!-- BEGIN HIER MET HET BOUWEN VAN DE USER INTERFACE VOOR HET LOGIN SCHERM -->
 <!-- GEBRUIK HIERVOOR DE SCREENSHOT DIE IN TRELLO STAAT EN BOUW DIE NA SUCCES! -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hey!</h1>
+</body>
+</html>
+
