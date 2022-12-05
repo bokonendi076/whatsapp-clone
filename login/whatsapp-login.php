7a4c8e39a24fc5a00c7c51cedc5876adb6e8bb1f
bokonendi076 <63402965+bokonendi076@users.noreply.github.com>
2022-12-05 10:05:39 +0100

    form badge message & form fixes

--- login/whatsapp-login.php
+++ login/whatsapp-login.php
@@ -20,7 +20,7 @@ require_once dirname(__DIR__) . './header.php';
         <div class="form-tutorial">
             <h1 class="login-form-title">Login om WhatsApp op uw <br> computer te gebruiken.</h1>
             <ol>
-                <li>Open WhatsApp op uw webbrowser</li>
+                <li>Open WhatsApp op uw webbrowser.</li>
                 <li>Vul uw gegevens in in het <b>Formulier</b></li>
                 <li>Tik vervolgens op Aanmelden</li>
                 <li>Geniet van <b>WhatsApp</b> op uw browser</li>
